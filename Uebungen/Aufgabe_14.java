5ec242c11088a63c6d34a68d8d03f4d38d018326
OSALD2000 <os14052020@gmail.com>
2024-04-30 09:59:20 +0200

    03.04.2024

--- Uebungen/Aufgabe_14.java
+++ Uebungen/Aufgabe_14.java
@@ -1,125 +1,150 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.Semaphore;
+import java.util.stream.Stream;
 
-class Resource extends Semaphore
-{
-    private volatile Integer resource;
-    private volatile boolean done = false;
+class Resource {
+    private Integer resource;
+    private Semaphore semaphore;
 
-    public Resource(int permits, boolean fair)
-    {
-        super(permits, fair);
+
+    private boolean done;
+    private boolean filled;
+
+    public Resource() {
+        this.filled = false;
+        this.done = false;
+        semaphore = new Semaphore(1, true);
     }
 
+    public synchronized boolean write(Integer resource) {
+        boolean operated = false;
 
-    public void schreibeResource(int wert)
-    {
         try {
-            this.acquire();
-            resource = wert;
+            this.semaphore.acquire();
+            if (!this.isFilled()) {
+                this.resource = resource;
+                int time = (int) (Math.random() * 500);
+                System.out.println("writing ----> " + this.resource + " need time : " + time);
+                Thread.sleep(time);
+                this.setFilled(true);
+                operated = true;
+                System.out.println("Value  " + resource + " ist in Resource geschrieben!!");
+            }
+            this.semaphore.release();
+            return operated;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println(wert + " ist in Speicherplazt!!");
-        this.release();
     }
 
-    public void verbraucherResource(ArrayList<Integer> arrayList)
-    {
+    public synchronized Integer read() {
+        Integer data = null;
         try {
-            this.acquire();
-            arrayList.add(resource);
-            System.out.println(resource + " wurde verbrauct !!");
-
-            resource = null;
+            this.semaphore.acquire();
+            if (this.isFilled()) {
+                data = this.resource;
+                this.setFilled(false);
+            }
+            this.semaphore.release();
+            return data;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        this.release();
     }
 
-    public void setDone(boolean done)
-    {
-        this.done = done;
+
+    public synchronized boolean isFilled() {
+        return this.filled;
     }
 
-    public Integer getResource(){
-        return resource;
+    public synchronized void setFilled(boolean filled) {
+        this.filled = filled;
     }
 
     public boolean isDone() {
         return done;
     }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
 }
 
-class Erzeuger extends Thread
-{
+
+class Erzeuger extends Thread {
+
     private Resource r;
-    private int[] resourceArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    private ArrayList<Integer> items;
+
+    @Override
+    public void run() {
+        for (Integer i : this.items)
+        {
+            while (!r.write(i)) /*wait*/;
+        }
+        r.setDone(true);
+        System.out.println("Erzeuger ist fertig!!");
+    }
+
+    public Erzeuger(Resource r, int numberOfItem) {
+        this.r = r;
+        this.items = new ArrayList<>() {{
+            for (int i = 0; i < numberOfItem; i++) {
+                add(getRandomNumber());
+            }
+        }};
+        System.out.println("Erzeuger elements !!");
+        this.items.forEach(i -> System.out.print(i + " "));
+        System.out.println();
+    }
+
+    private int getRandomNumber() {
+        return (int) (Math.random() * 100);
+    }
+}
+
+class Verbraucer extends Thread {
+
+    private Resource r;
+    private ArrayList<Integer> items;
 
     @Override
     public void run()
     {
-        for (int i : resourceArray)
+        while (!r.isDone())
         {
-
-            while (r.getResource() != null)/*wait*/;
-            System.out.println("Schreibe " + i + " in Resource ");
-            r.schreibeResource(i);
+            Integer i = r.read();
+            if(i != null)
+            {
+                this.items.add(i);
+                System.out.println("Verbraucer hat " + i + " in Resource gebraucht!!");
+            }
         }
 
-        r.setDone(true);
-
-        System.out.println("Erzeuger Done");
+        System.out.println("Verbraucer ist fertig!!");
+        System.out.println("bearbeitete Resourcen : ");
+        this.items.forEach(i -> System.out.print(i + " "));
     }
 
-    public Erzeuger(Resource r){
+    public Verbraucer(Resource r) {
         this.r = r;
-    }
-
-
-}
-
-class Verbraucer extends Thread
-{
-
-    private Resource r;
-    private ArrayList<Integer> arrayList;
-
-    @Override
-    public void run()
-    {
-            while (r.getResource() == null) /*wait*/;
-
-            while(!r.isDone()){
-                r.verbraucherResource(this.arrayList);
-            }
-
-            System.out.println("Verbraucher Done !!");
-    }
-
-    public Verbraucer(Resource r){
-        this.r = r;
-        this.arrayList = new ArrayList<>();
+        this.items = new ArrayList<>();
     }
 
 }
 
 
-public class Aufgabe_14
-{
+public class Aufgabe_14 {
 
-    public static void main(String[] args)
-    {
-        Resource r = new Resource(1, true);
+    public static void main(String[] args) {
+        Resource r = new Resource();
         Verbraucer v = new Verbraucer(r);
-        Erzeuger e = new Erzeuger(r);
+        Erzeuger e = new Erzeuger(r , 250);
 
         e.start();
         v.start();
-
-
-
     }
 }
